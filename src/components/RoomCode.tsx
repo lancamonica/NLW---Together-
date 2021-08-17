9b4e690eb15b71309672b678cd1b0b5d7686b13b
unknown <RIB-PRX-17@RIB-PRX-17.PREMIX.INTRANET>
2021-08-17 16:07:16 -0300

    feat: Continuing the creation of the question screen for users and making it responsive

--- src/components/RoomCode.tsx
+++ src/components/RoomCode.tsx
@@ -2,18 +2,25 @@
 import copyImg from '../assets/images/copy.svg';
 //styled
 import '../styles/room-code.scss';
+import GridItem from './GridItem';
 
-export function RoomCode() {
-  /* function copyRoomCodeToClipboard() {
-    navigator.clipboard.writeText('-MdIK6AHxfgv8m1eLZ16')
-  } */
+type RoomCodeProps = {
+  code: string;
+}
+
+export function RoomCode(props: RoomCodeProps) {
+  function copyRoomCodeToClipboard() {
+    navigator.clipboard.writeText(props.code)
+  } 
 
   return (
-    <button className="room-code">
-      <div>
-        <img src={copyImg} alt="Copy room code" />
-      </div>
-      <span>Sala #2367367364736</span>
-    </button>
+    <GridItem item xs={7} sm={6} md={4} lg={4} xl={5}>
+      <button className="room-code" onClick={copyRoomCodeToClipboard}>
+        <div>
+          <img src={copyImg} alt="Copy room code" />
+        </div>
+        <span>Sala {props.code}</span>
+      </button>
+    </GridItem>
   );
 }
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,37 +1,136 @@
 //internal
+import { useEffect } from 'react';
+import { FormEvent, useState } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
+import { useParams } from 'react-router-dom';
 import logoImg from '../assets/images/logo.svg';
 import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode';
+import { useAuth } from '../hooks/useAuth';
+import { database } from '../service/firebase';
 //styled
 import '../styles/room.scss';
 
+type FirebaseQuestions = Record<string, {
+  author: {
+    name: string;
+    avatar: string;
+  }
+  content: string;
+  isAnswered: boolean;
+  isHighLighted: boolean;
+}>
+
+type Question = {
+  id: string;
+  author: {
+    name: string;
+    avatar: string;
+  }
+  content: string;
+  isAnswered: boolean;
+  isHighLighted: boolean;
+}
+
+type RoomParams = {
+  id: string;
+}
+
 export function Room() {
+  const params = useParams<RoomParams>();
+  const roomId = params.id;
+  const { user } = useAuth();
+  const [newQuestion, setNewQuestion] = useState('');
+  const [questions, setQuestions] = useState<Question[]>([]);
+  const [title, setTitle] = useState('');
+
+  useEffect(() => {
+    const roomRef = database.ref(`rooms/${roomId}`);
+
+    roomRef.on('value', room => {
+      const databaseRoom = room.val();
+      const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
+      const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {
+        return {
+          id: key,
+          content: value.content,
+          author: value.author,
+          isHighLighted: value.isHighLighted,
+          isAnswered: value.isAnswered,
+        }
+      });
+
+      setTitle(databaseRoom.title);
+      setQuestions(parsedQuestions)
+    });
+  }, [roomId]);
+
+  async function handleSendQuestion(event: FormEvent) {
+    event.preventDefault();
+
+    if (newQuestion.trim() === '') {
+      return
+    }
+
+    if (!user) {
+      toast.error('You must be logged in');
+      return
+    }
+
+    const question = {
+      content: newQuestion,
+      author: {
+        name: user?.name,
+        avatar: user?.avatar,
+      },
+      isHighLighted: false,
+      isAnswered: false,
+    };
+
+    await database.ref(`rooms/${roomId}/questions`).push(question);
+
+    setNewQuestion('');
+  }
   return (
     <div id="page-room">
       <header>
         <div className="content">
           <img src={logoImg} alt="Letmeask" />
-          <RoomCode />
+          <RoomCode code={roomId} />
         </div>
       </header>
 
       <main>
         <div className="room-title">
-          <h1>Sala React</h1>
-          <span>4 perguntas</span>
+          <h1>Sala {title}</h1>
+          {questions.length > 0 && (
+            <span>{questions.length} pergunta(s)</span>
+          )}
         </div>
 
-        <form>
+        <form onSubmit={handleSendQuestion}>
           <textarea 
             placeholder="O que você quer perguntar?"
+            onChange={(event) => setNewQuestion(event?.target.value)}
+            value={newQuestion}
           />
 
           <div className="form-footer">
-            <span>Para enviar uma perginta, <button>faça seu login</button>.</span>
-            <Button type="submit">Enviar pergunta</Button>
+            { user ? (
+              <div className="user-info">
+                <img src={user.avatar} alt={user.name}/>
+                <span>{user.name}</span>
+              </div>
+            ) : (
+              <span>Para enviar uma perginta, <button>faça seu login</button>.</span>
+            )}
+            <Button type="submit" disabled={!user} >Enviar pergunta</Button>
           </div>
         </form>
+        {JSON.stringify(questions)}
       </main>
+
+      <div><Toaster position="top-right"reverseOrder={true}/></div>
     </div>
   );
 }
